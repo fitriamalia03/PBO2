4450994ea1825b3a911cda43e39b394f2a6d5286
fitriamalia03 <127418446+fitriamalia03@users.noreply.github.com>
2023-03-31 21:46:27 +0700

    Add files via upload

---
+++ Praktikum3/overload1.py
@@ -0,0 +1,6 @@
+# Nama  : Fitri Amalia Nurfathir
+# Kelas : R1
+# NIM   : 210511005
+
+print(len("geeks"))
+print(len([10, 20, 30]))
---
+++ Praktikum3/overload2.py
@@ -0,0 +1,7 @@
+# Nama  : Fitri Amalia Nurfathir
+# Kelas : R1
+# NIM   : 210511005
+
+print(5 - 1)
+print("Above" + " The" + " Clouds")
+print([1, 2] + [3, 4]) 
