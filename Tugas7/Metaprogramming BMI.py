a716f3b9e00e98c2d66a5f8c48339170e6129b01
fitriamalia03 <127418446+fitriamalia03@users.noreply.github.com>
2023-05-21 23:19:52 +0700

    Add files via upload

---
+++ Tugas7/Metaprogramming BMI.py
@@ -0,0 +1,24 @@
+# Nama  : Fitri Amalia Nurfathir
+# Kelas : R1
+# NIM   : 210511005
+
+class MenghitungBMIMeta(type):
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+        
+        def pria(cls, tinggi):
+            return (tinggi - 100) - (tinggi - 100) * 10/100
+        cls.pria = classmethod(pria)
+
+        def wanita(cls, tinggi):
+            return (tinggi - 100) - (tinggi - 100) * 15/100
+        cls.wanita = classmethod(wanita)
+
+class Ideal(metaclass=MenghitungBMIMeta):
+    pass
+
+A = Ideal()
+B = A.pria(175)
+C = A.wanita(155)
+print('BMI Pria:',B)
+print('BMI Wanita:',C)
